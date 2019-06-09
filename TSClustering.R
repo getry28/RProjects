6368dd4547edfc34a4bb8b085d5e31fd52826f8f
getry28 <34693052+getry28@users.noreply.github.com>
2019-06-09 19:59:35 +0200

    TS clustering analysis updated

--- TSClustering.R
+++ TSClustering.R
@@ -5,11 +5,18 @@
 library(xlsx)
 library(httr)
 library(psych)
+library(tidyr)
 library(dplyr)
 library(tseries)
 library(ggplot2)
 library(TSclust)
+library(ggcorrplot)
 library(dtwclust)
+library(clValid)
+library(dendextend)
+
+
+# Data import ----
 
 #data import
 url <- "https://github.com/getry28/RProjects/blob/master/MAThesis_Data.xlsx?raw=true"
@@ -21,27 +28,54 @@ Data2018 <- read.xlsx("MAThesis_Data.xlsx", sheetIndex=5, header=TRUE)%>%
 labels<-colnames(Data2017%>%
                    select(-1))
 
+
+# Descriptive statistics ----
+
 #descriptive statistics - 2017
 DS2017<-describe(Data2017%>%
                    select(-Data))%>%
   select(-c('vars','n','trimmed','mad','se'))
+colnames(DS2017)<-c("Srednia","Odchylenie","Mediana","MIN","MAX","Rozstep",
+                    "Skośność","Kurtoza")
 
 #descriptive statistics - 2018
 DS2018<-describe(Data2018%>%
                    select(-Data))%>%
   select(-c('vars','n','trimmed','mad','se'))
+colnames(DS2018)<-c("Srednia","Odchylenie","Mediana","MIN","MAX","Rozstep",
+                    "Skośność","Kurtoza")
+
+#desriptive statisctics analysis - tables for export
+DS2018A<-describe(DS2018)%>%
+  select(-c('vars','n','trimmed','mad','se'))%>%
+  round(digit=5)
+
+DS2017A<-describe(DS2017)%>%
+  select(-c('vars','n','trimmed','mad','se'))%>%
+  round(digit=5)
 
 #descriptive statistics 2017 boxplots
-#chyba że zrobić np. histogram z funkcją gęstości - DO DOPYTANIA
-boxplot(DS2017$mean, horizontal=T, xlab="Wartości średniej")
+boxplot(DS2017$mean, DS2018$mean, horizontal=T, xlab="Wartości średniej",
+        names=c("2017", "2018"), las=1, col=c("orange","red"),
+        border="brown")
 
-boxplot(DS2017$sd, horizontal = T, xlab="Wartości odchylenia standardowego")
+boxplot(DS2017$sd, DS2018$sd, horizontal=T, 
+        xlab="Wartości odchylenia standardowego",
+        names=c("2017", "2018"), las=1, col=c("orange","red"),
+        border="brown")
 
-#boxplot(DS2017$median, horizontal=T, xlab="Wartości mediany")
+boxplot(DS2017$skew, DS2018$skew, horizontal=T, 
+        xlab="Wartości współczynnika skośności",
+        names=c("2017", "2018"), las=1, col=c("orange","red"),
+        border="brown")
 
-boxplot(DS2017$skew, horizontal=T, xlab="Wartości współczynnika skośności")
+boxplot(DS2017$Kurtoza, DS2018$Kurtoza, horizontal=T, xlab="Wartości kurtozy",
+        names=c("2017", "2018"), las=1, col=c("orange","red"),
+        border="brown")
 
-boxplot(DS2017$kurtosis, horizontal=T, xlab="Wartości kurtozy")
+
+# Autocorrelation analysis ----
+
 
 #ACF - lag 1 - 2017
 ACF2017<-acf(Data2017%>%
@@ -54,19 +88,6 @@ DSACF2017<-describe(ACF1)%>%
   select(-c('vars','n','trimmed','mad','se'))
 rownames(DSACF2017)<-labels
 
-boxplot(DSACF2017$mean, horizontal=T, xlab="Wartości współczynnika autokorelacji pierwszego rzędu")
-
-#descriptice statitstics 2018 boxplots
-boxplot(DS2018$mean, horizontal=T, xlab="Wartości średniej")
-
-boxplot(DS2018$sd, horizontal = T, xlab="Wartości odchylenia standardowego")
-
-#boxplot(DS2018$median, horizontal=T, xlab="Wartości mediany")
-
-boxplot(DS2018$skew, horizontal=T, xlab="Wartości współczynnika skośności")
-
-boxplot(DS2018$kurtosis, horizontal=T, xlab="Wartości kurtozy")
-
 #ACF - lag 1 - 2018
 ACF2018<-acf(Data2018%>%
                select(-Data), lag.max=1, plot=F)
@@ -78,25 +99,72 @@ DSACF2018<-describe(ACF2)%>%
   select(-c('vars','n','trimmed','mad','se'))
 rownames(DSACF2018)<-labels
 
-boxplot(DSACF2018$mean, horizontal=T, xlab="Wartości współczynnika autokorelacji pierwszego rzędu")
+#ACF significance test
+ACFtest<-as.data.frame(matrix(0,68,4))
+rownames(ACFtest)<-labels
+colnames(ACFtest)<-c("2017","2018","Significance 2017","Significance 2018")
 
-#TS clustering - 2017
+ACFtest$`2017`<-DSACF2017$mean*sqrt(NROW(DSACF2017$mean))
+ACFtest$`2018`<-DSACF2018$mean*sqrt(NROW(DSACF2018$mean))
+
+CIup<-1.96/sqrt(68)
+CIdown<-(-1.96/sqrt(68))
+
+ACFtest$`Significance 2017`<-ifelse(ACFtest$`2017`<CIup & ACFtest$`2017`>CIdown,1,0)
+ACFtest$`Significance 2018`<-ifelse(ACFtest$`2018`<CIup & ACFtest$`2018`>CIdown,1,0)
+
+#ACF boxplots
+boxplot(DSACF2017$Srednia,DSACF2018$Srednia, horizontal=T, 
+        xlab="Wartości współczynnika autokorelacji pierwszego rzędu",
+        names=c("2017", "2018"), las=1, col=c("orange","red"),
+        border="brown")
+
+#ACF density plots
+DD<-cbind(DSACF2017$mean,DSACF2018$mean)%>%
+  as.data.frame()
+colnames(DD)<-c("2017", "2018")
+DD2<-gather(DD,Rok, ACF)
+
+ggplot(DD2, aes(x=ACF, color=Rok))+
+         geom_density(size=1.1)+
+  ylab("Gęstość")+
+  xlab("Wartości współczynnika autokorelacji pierwszego rzędu")
+
+
+# TS Clustering 2017 ----
+
 #COR distance
 D1<-diss(Data2017%>% select(-Data),"COR")
-D1H<-hclust(D1, method="ward.D")%>%
-  plot(hang=-1, cex=0.8)
-D1<-as.data.frame(as.matrix(D1))
+D1H<-hclust(D1, method="ward.D")
+plot(D1H,hang=-1, cex=0.8)
+
+#correlation analysis
+COR2017<-cor(Data2017%>%
+               select(-Data))%>%
+  round(digit=2)
+
+COR2017I<-describe(COR2017)%>%
+  select(-c('vars','n','trimmed','mad','se'))%>%
+  round(digit=2)
+
+ggplot(COR2017I, aes(x=mean))+
+  geom_histogram(binwidth=0.005, color="black", fill="black")+
+  geom_density(size=1.1, color="#2c7fb8")+
+  xlab("Wartości współczynników korelacji")+
+  ylab("Gęstość")
+
+p.mat<-cor_pmat(COR2017)
+ggcorrplot(COR2017, type="upper", outline.col="white", p.mat=p.mat)
 
 #AR distance
-D2<-diss(Data2017%>% select(-Data),"AR.MAH")
-D2H<-hclust(D2[["p_value"]],method="ward.D")
-D2H[["labels"]]<-labels
+D2<-diss(Data2017%>% select(-Data),"AR.PIC")
+D2H<-hclust(D2,method="ward.D")
 plot(D2H,hang=-1, cex=0.8)
 
 #periodogram distance
 D3<-diss(Data2017%>% select(-c(Data,PBG)),"PER")
-D3H<-hclust(D3, method="ward.D")%>%
-  plot(hang=-1, cex=0.8)
+D3H<-hclust(D3, method="ward.D")
+plot(D3H,hang=-1, cex=0.8)
 
 #group periodograms
 #group 1
@@ -121,23 +189,41 @@ spectrum(Data2017%>%
                     PGE,TAURON,ENEA,PGNIG,FERRO,ORANGE,K2.INTERNET,PFLEIDERER,GRUPA.AZOTY,
                     KOMPUTRONIK,WAWEL,AGORA,COMARCH,HENRYK.KANIA,KGHM,TARCZYNSKI)))
 
-#TS clustering - 2018
+
+# TS Clustering 2018 ----
+
 #COR distance
 D10<-diss(Data2018%>% select(-Data),"COR")
-D10H<-hclust(D10, method="ward.D")%>%
-  plot(hang=-1, cex=0.8)
+D10H<-hclust(D10, method="ward.D")
+plot(D10H,hang=-1, cex=0.8)
 
+#correlation analysis
+COR2018<-cor(Data2018%>%
+               select(-Data))%>%
+  round(digit=2)
+
+COR2018I<-describe(COR2018)%>%
+  select(-c('vars','n','trimmed','mad','se'))%>%
+  round(digit=2)
+
+ggplot(COR2018I, aes(x=mean))+
+  geom_histogram(binwidth=0.005, color="black", fill="black")+
+  geom_density(size=1.1, color="#2c7fb8")+
+  xlab("Wartości współczynników korelacji")+
+  ylab("Gęstość")
+
+p.mat2<-cor_pmat(COR2018)
+ggcorrplot(COR2018, type="upper", outline.col="white", p.mat=p.mat2)
 
 #AR distance
-D11<-diss(Data2018%>% select(-Data),"AR.MAH")
-D11H<-hclust(D11[["p_value"]],method="ward.D")
-D11H[["labels"]]<-labels
-  plot(D11H, hang=-1,cex=0.8)
+D11<-diss(Data2018%>% select(-Data),"AR.PIC")
+D11H<-hclust(D11,method="ward.D")
+plot(D11H,hang=-1,cex=0.8)
 
 #periodogram distance
 D12<-diss(Data2018%>% select(-c(Data,PBG)),"PER")
-D12H<-hclust(D12, method="ward.D")%>%
-  plot(hang=-1, cex=0.8)
+D12H<-hclust(D12, method="ward.D")
+plot(D12H,hang=-1, cex=0.8)
 
 #group periodograms
 #group 1
@@ -159,3 +245,85 @@ spectrum(Data2017%>%
 spectrum(Data2017%>%
            select(SKYLINE,BNP.PARIBAS,MZN.PROPERTY,INTER.SPORT,HENRYK.KANIA,CD.PROJEKT,GRUPA.AZOTY,
                   MOSTOSTAL.WAWA,X11.BIT,KRAKCHEMIA,SYGNITY,ELZAB,PRIME.CAR.MANAGEMENT))
+
+
+# TS Sq Clustering 2017 ----
+
+Data2017SQ<-(Data2017%>%
+  select(-Data))^2%>%
+  round(digit=6)
+Data2017SQ<-cbind(Data2017$Data,Data2017SQ)
+names(Data2017SQ)[1]<-"Data"
+
+D100<-diss(Data2017SQ%>% select(-Data),"COR")
+D100H<-hclust(D100, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+D101<-diss(Data2017SQ%>% select(-Data),"AR.PIC")
+D101H<-hclust(D101, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+D102<-diss(Data2017SQ%>% select(-c(Data,PBG)),"PER")
+D102H<-hclust(D102, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+# TS Sq Clustering 2018 ----
+Data2018SQ<-(Data2018%>%
+               select(-Data))^2%>%
+  round(digit=6)
+Data2018SQ<-cbind(Data2018$Data,Data2018SQ)
+names(Data2018SQ)[1]<-"Data"
+
+D110<-diss(Data2018SQ%>% select(-Data),"COR")
+D110H<-hclust(D110, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+D111<-diss(Data2018SQ%>% select(-Data),"AR.PIC")
+D111H<-hclust(D111, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+D112<-diss(Data2018SQ%>% select(-Data),"PER")
+D112H<-hclust(D112, method="ward.D")%>%
+  plot(hang=-1, cex=1)
+
+
+# Dendrograms comparison ----
+
+#correlation distance
+dend_listCOR<-dendlist(as.dendrogram(D1H), as.dendrogram(D10H))
+
+dendlist(as.dendrogram(D1H), as.dendrogram(D10H))%>%
+  untangle(method="step1side")%>%
+  tanglegram()
+
+dendlist(as.dendrogram(D1H), as.dendrogram(D10H))%>%
+  untangle(method="step1side")%>%
+  entanglement()
+
+cor.dendlist(dend_listCOR, method="cophenetic")
+
+#AR distance
+dend_listAR<-dendlist(as.dendrogram(D2H), as.dendrogram(D11H))
+
+dendlist(as.dendrogram(D2H), as.dendrogram(D11H))%>%
+  untangle(method="step1side")%>%
+  tanglegram()
+
+dendlist(as.dendrogram(D2H), as.dendrogram(D11H))%>%
+  untangle(method="step1side")%>%
+  entanglement()
+
+cor.dendlist(dend_listAR, method="cophenetic")
+
+#periodogram distance
+dend_listPER<-dendlist(as.dendrogram(D3H), as.dendrogram(D12H))
+
+dendlist(as.dendrogram(D3H), as.dendrogram(D12H))%>%
+  untangle(method="step1side")%>%
+  tanglegram()
+
+dendlist(as.dendrogram(D3H), as.dendrogram(D12H))%>%
+  untangle(method="step1side")%>%
+  entanglement()
+
+cor.dendlist(dend_listAR, method="cophenetic")
